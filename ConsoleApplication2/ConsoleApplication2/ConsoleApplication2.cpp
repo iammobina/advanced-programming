6b279b6e8bcde720287f6fad35bb304a58d325fa
mobina kashanian <mobina.km@gmail.com>
2018-02-25 23:16:22 +0000

    teeestttt

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,21 @@
+// ConsoleApplication2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main(int argc, char* argv[])
+	{
+	while (true)
+	{
+		char* charr = new char[1000];
+		//Sleep(1);
+		//delete[] charr;
+	}
+		
+		return 0;
+	}
+
+
+    
---
+++ CsharpReview/CsharpReview/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+
+namespace CsharpReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                char[] ch = new char[1000];
+                //char* charr = new charr[100];
+                 Thread.Sleep(1);
+                 //delete[] charr;
+            }
+           
+        }
+    }
+}
