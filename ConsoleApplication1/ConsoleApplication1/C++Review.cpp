fda397959d74c26ccb0ee00cf41034ab55700731
mobina kashanian <mobina.km@gmail.com>
2018-02-26 10:55:54 +0000

    hhh

--- ConsoleApplication1/ConsoleApplication1/C++Review.cpp
+++ ConsoleApplication1/ConsoleApplication1/C++Review.cpp
@@ -1,16 +1,12 @@
-#include "stdafx.h"
 #include <iostream>
-
-
+using namespace std;
 int main(int argc, char* argv[])
-	{
+{
 	while (true)
 	{
 		char* charr = new char[1000];
+		//Sleep(1);
 		delete[] charr;
 	}
-		return 0;
-	}
-
-
-    
+	return 0;
+}
