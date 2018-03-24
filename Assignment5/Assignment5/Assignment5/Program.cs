ffa550736461be93df07d28aefe31d96630d1b6f
mobina kashanian <mobina.km@gmail.com>
2018-03-24 23:41:44 +0000

    this is my assignment5

---
+++ Assignment5/Assignment5/Assignment5/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+namespace Assignment5
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            RecipeBook fromMom = new RecipeBook("دستور پخت های مادر", 20);
+
+            ConsoleKeyInfo cki;
+            do
+            {
+                Console.WriteLine($"Press N(ew), D(el), S(earch)or L(ist)");
+                cki = Console.ReadKey();
+                Console.WriteLine();
+                
+                switch (cki.Key)
+                {
+                    case ConsoleKey.N:
+                        Console.WriteLine("New Recipe");
+                        // بر عهده دانشجو
+                        Recipe foods = GetRecipeFromInput();
+                        fromMom.Add(foods);
+                        Console.WriteLine("Your recipe added successfully !");
+                        break;
+                    case ConsoleKey.D:
+                        Console.WriteLine("Delete Recipe");
+                        // بر عهده دانشجو
+   
+                        Console.WriteLine("Are you sure you want to delete your recipe? press Y(for Yes) OR  N(for No)");
+                        cki = Console.ReadKey();
+                        Console.WriteLine();
+                        switch (cki.Key)
+                        {
+                            case ConsoleKey.Y:
+                                Console.WriteLine("Plese Enter the title of Recipe");
+                                fromMom.Remove(Console.ReadLine());
+                                Console.WriteLine("Your recipe removed successfully !");
+                                break;
+                            case ConsoleKey.N:
+                                Console.WriteLine("Your recipe still exist !");
+                                break;
+                        }
+                        break;
+                    case ConsoleKey.S:
+                        Console.WriteLine("Search Recipe");
+                        // بر عهده دانشجو
+                        Console.WriteLine("How do you want to search? By cuisine(C) or keyword(K) or title(T)?");
+                        cki = Console.ReadKey();
+                        Console.WriteLine();
+                        switch (cki.Key)
+                        {
+                            
+                            case ConsoleKey.C:
+                                Console.WriteLine($"Please enter a cuisine that you want to search:");
+                                Show(fromMom.LookupByCuisine(Console.ReadLine()));
+                                break;
+                            case ConsoleKey.K:
+                                Console.WriteLine($"Please enter a keyword that you want to search");
+                                Show(fromMom.LookupByKeyword(Console.ReadLine()));
+                                break;
+                            case ConsoleKey.T:
+                                Console.WriteLine($"Please enter a title that you want to search:");
+                                Show(fromMom.LookupByTitle(Console.ReadLine()));
+                                break;
+                        }
+                        break;
+                    case ConsoleKey.L:
+                        Console.WriteLine("List Recipes");
+                        // بر عهده دانشجو
+                        for (int i = 0; i < fromMom.listrecipe.Length; i++)
+                        {
+                            if (fromMom.listrecipe[i] == null)
+                            continue;
+                            else
+                                Show(fromMom.listrecipe[i]);
+                        }
+                        break;
+                    case ConsoleKey.Escape:
+                        Console.WriteLine("Esc");
+                        break;
+                    default:
+                        Console.WriteLine($"Invalid Key: {cki.KeyChar}");
+                        break;
+                }
+
+                Console.WriteLine("Press any key to continue, Esc to exit");
+                cki = Console.ReadKey();
+                Console.Clear();
+            }
+            while (cki.Key != ConsoleKey.Escape);
+        }
+        public static bool Show(Recipe recipe)
+        {
+             Console.WriteLine(recipe.ToString());
+            return true;
+        }
+
+        public static Recipe GetRecipeFromInput()
+        {
+            Console.WriteLine("Please name the food :");
+            string title = Console.ReadLine();
+            Console.WriteLine("Please write about this food:");
+            string description = Console.ReadLine();
+            Console.WriteLine("How many ingredients do you have?");
+            int ingCount = int.Parse(Console.ReadLine());
+            Console.WriteLine("How many people can use this food?");
+            int servCount = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please write about cuisine of the food:");
+            string cuisine = Console.ReadLine();
+            Console.WriteLine("Please Enter a Keyword:)");
+            string[] keywords = Console.ReadLine().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            
+            Ingredient[] eachnewingredient = new Ingredient [ingCount];
+            for (int i = 0; i < ingCount; i++)
+            {
+                Console.WriteLine("Please name the ingredient:");
+                string name = Console.ReadLine();
+                Console.WriteLine("Please write about this ingredient:");
+                string ingdescription = Console.ReadLine();
+                Console.WriteLine("Please write about it's quantity:");
+                double quantity = double.Parse(Console.ReadLine());
+                Console.WriteLine("Please write about it's unit:");
+                string unit = Console.ReadLine();
+                eachnewingredient[i] = new Ingredient(name, ingdescription, quantity, unit);
+            }
+            Recipe foods = new Recipe(title, description, servCount, ingCount, cuisine, keywords);
+            for (int i = 0; i < ingCount; i++)
+                foods.AddIngredient(eachnewingredient[i]);
+            return foods;
+
+        }
+    }
+}
---
+++ Assignment5/Assignment5/Assignment5/Recipe.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    /// <summary>
+    /// دستور پخت 
+    /// </summary>
+    public class Recipe
+    {
+        
+
+        /// <summary>
+        /// ایجاد دستور پخت جدید
+        /// </summary>
+        /// <param name="title">عنوان</param>
+        /// <param name="instructions">دستورات</param>
+        /// <param name="ingredients">لیست مواد مورد نیاز</param>
+        /// <param name="servingCount">تعداد افراد</param>
+        /// <param name="cuisine">سبک غذا</param>
+        /// <param name="keywords">کلمات کلیدی</param>
+        public Recipe(string title, string instructions, Ingredient[] ingredients, int servingCount, string cuisine, string[] keywords)
+        {
+            // بر عهده دانشجو
+            this.Title = title;
+            Instructions = instructions;
+            ingredientlist = ingredients;
+            IngredientCount = ingredients.Length;
+            this.ServingCount = servingCount;
+            this.Cuisine = cuisine;
+            this.Keyword = keywords;
+        }
+
+        /// <summary>
+        /// ایجاد شئ دستور پخت جدید
+        /// </summary>
+        /// <param name="title">عنوان</param>
+        /// <param name="instructions">دستورات</param>
+        /// <param name="ingredientCount">تعداد مواد مورد نیاز</param>
+        /// <param name="servingCount">تعداد افراد</param>
+        /// <param name="cuisine">سبک غذا</param>
+        /// <param name="keywords">کلمات کلیدی</param>
+        public Recipe(string title, string instructions, int ingredientCount, int servingCount, string cuisine, string[] keywords)
+        {
+            // بر عهده دانشجو
+            this.Title = title;
+            this.Instructions = instructions;
+            this.IngredientCount = ingredientCount;
+            this.ServingCount = servingCount;
+            this.Cuisine = cuisine;
+            this.Keyword = keywords;
+            ingredientlist = new Ingredient[ingredientCount];
+        }
+        public int ingcount { get; set; }
+        public string instructions { get; set; }
+        public Ingredient[] ingredientCount { get; set; }
+        public int servingcount { get; set; }
+        public string[] keyword { get; set; }
+
+        /// <summary>
+        /// اضافه کردن ماده اولیه 
+        /// </summary>
+        /// <param name="ingredient">ماده اولیه</param>
+        /// <returns>عمل اضافه کردن موفقیت آمیز انجام شد یا خیر. در صورت تکمیل ظرفیت مقدار برگشتی "خیر" میباشد.</returns>
+        public bool AddIngredient(Ingredient ingredient)
+        {
+            // بر عهده دانشجو
+            for (int i=0;i<ingredientlist.Length;i++)
+            {
+                if(ingredientlist[i] == null)
+                {
+                    ingredientlist[i] = ingredient;
+                    return true;
+                }
+
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// حذف تمام مواد اولیه که با نام ورودی تطبیق میکند
+        /// </summary>
+        /// <param name="name">نام ماده اولیه برای حذف</param>
+        /// <returns>آیا حداقل یک ماده اولیه حذف شد؟</returns>
+        public bool RemoveIngredient(string name)
+        {
+            // بر عهده دانشجو
+            for (int i = 0; i < ingredientlist.Length; i++)
+            {
+                if (ingredientlist[i].Name == name)
+                {
+                    ingredientlist[i] = null;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// بروز کردن تعداد افرادی که این دستور غذا برای آن تعداد مناسب است
+        /// مقادیر مواد اولیه به نسبت لازم اضافه میشود
+        /// </summary>
+        /// <param name="newServingCount">تعداد افراد جدید</param>
+        public void UpdateServingCount(int newServingCount)
+        {
+            // بر عهده دانشجو
+            for (int i = 0; i < ingredientlist.Length; i++)
+            {
+                Ingredients[i].Quantity = (Ingredients[i].Quantity * newServingCount);
+            }
+            ServingCount = newServingCount;
+            
+        }
+
+        /// <summary>
+        /// فیلد پیشتیبان برای Ingredients.
+        /// </summary>
+        private Ingredient[] _Ingredients;
+
+        /// <summary>
+        /// مواد اولیه
+        /// </summary>
+        public Ingredient[] Ingredients {
+            get
+            {
+                return _Ingredients;
+            }
+            private set
+            {
+                // بر عهده دانشجو
+                            
+            }
+        }
+
+        public string[] Keyword { get;  set; }
+        public string Cuisine { get; set; }
+        public int ServingCount { get;  set; }
+        public int IngredientCount { get;  set; }
+        public string Instructions { get;  set; }
+        public string Title { get;  set; }
+        public Ingredient[] ingredientlist { get;  set; }
+        public int servingcounter { get; set; }
+
+        public override string ToString()
+        {
+            // بر عهده دانشجو
+            return $"Recipe Name :{Title}" +
+                $"-describtion :{Instructions}  " +
+                $"-cuisine: {Cuisine} " +
+                $" -ServingCount : {ServingCount} " +
+                $"-keyword{Keyword}";
+            
+        }
+    }
+}
