0e74ed0fe9ed45a4b716496c852fdfa4d462b7cf
mobina kashanian <mobina.km@gmail.com>
2018-05-25 22:52:46 +0000

    A10

---
+++ Assigment10/Assigment10/BinaryOperator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace OOCalculator
+{
+    public abstract class BinaryOperator: Expression, IOperator
+    {
+        protected Expression LHS;
+        protected Expression RHS;
+
+        public BinaryOperator(TextReader reader)
+        {
+            LHS = Expression.GetNextExpression(reader);
+            RHS = Expression.GetNextExpression(reader);
+
+        }
+
+        public abstract string OperatorSymbol { get; }
+
+        public sealed override string ToString() => $"({this.LHS.ToString()} {this.OperatorSymbol} {this.RHS.ToString()})";
+
+    }
+}
---
+++ Assigment10/Assigment10/SubtractOperator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace OOCalculator
+{
+    public class SubtractOperator : BinaryOperator
+    {
+        public SubtractOperator(TextReader reader)
+            :base(reader)
+        {  }
+
+        public override string OperatorSymbol => "-";
+
+        public override double Evaluate() => this.LHS.Evaluate() - this.RHS.Evaluate();
+    }
+}
